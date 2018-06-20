017ec7e1b331cb6a80e5efc13e09d8e3cf7381a1
Barbara Washington <barbara.m.washington@gmail.com>
2018-06-20 22:22:11 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,7 +2,8 @@ def selects_all_female_bears_return_name_and_age
   "SELECT name, age FROM bears WHERE gender = 'F'"
 end
 
-def selects_all_bears_names_and_orders_in_alphabetical_order
+def selects_all_be
+  ars_names_and_orders_in_alphabetical_order
   "SELECT ASC(name) FROM bears"
 end
 
